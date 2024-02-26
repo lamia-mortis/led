861cec2b21c72deab8b2946ee62726d8b598db27
kris <lamia.mortis@icloud.com>
2024-02-26 12:10:58 +0100

    [00000-1] Replacing HAL with registers

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
-#include "main.h"
-#include "stm32f3xx_it.h"
+#include <stm32f3xx.h>
 
-/* Private function prototypes -----------------------------------------------*/
+#include "main.h"
+
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
+static void fill_modes_array();
 
 mode_function mode_functions[MODE_CNT] = {
     [MODE_WHEEL] = NULL,
@@ -13,109 +14,6 @@ mode_function mode_functions[MODE_CNT] = {
 };
 
 volatile led_mode_e current_mode = MODE_WHEEL;
-
-mode_function get_mode_function(led_mode_e mode)
-{
-    return mode_functions[mode];
-}
-
-void delay(uint32_t time)
-{
-    while (time--);
-}
-
-static void fill_modes_array()
-{
-    mode_functions[MODE_WHEEL] = apply_wheel_mode;
-    mode_functions[MODE_ALL_ON_OFF] = apply_all_on_off_mode;
-    mode_functions[MODE_SAME_COLOR] = apply_same_color_mode;
-    mode_functions[MODE_ODD_EVEN] = apply_odd_even_mode;
-}
-
-void apply_wheel_mode(void) 
-{
-    // to do implement custom drivers
-    
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_8, 1);
-    delay(100000);
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_9, 1);
-    delay(100000);
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_10, 1);
-    delay(100000);
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_11, 1);
-    delay(100000);
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_12, 1);
-    delay(100000);
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_13, 1);
-    delay(100000);
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_14, 1);
-    delay(100000);
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_15, 1);
-    delay(100000);
-
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_8, 0);
-    delay(100000);
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_9, 0);
-    delay(100000);
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_10, 0);
-    delay(100000);
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_11, 0);
-    delay(100000);
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_12, 0);
-    delay(100000);
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_13, 0);
-    delay(100000);
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_14, 0);
-    delay(100000);
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_15, 0);
-    delay(100000);
-}
-
-void apply_all_on_off_mode(void) 
-{
-    GPIO_PinState pin_state = HAL_GPIO_ReadPin(GPIOE, GPIO_PIN_12) == GPIO_PIN_RESET
-        ? GPIO_PIN_SET
-        : GPIO_PIN_RESET;
-
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_All, pin_state);
-    delay(1000000);
-}
-
-void apply_same_color_mode(void) 
-{
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_10|GPIO_PIN_14, 0);
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_9|GPIO_PIN_13, 1);
-
-    delay(1000000);
-
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_9|GPIO_PIN_13, 0);
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_8|GPIO_PIN_12, 1);
-
-    delay(1000000);
-
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_8|GPIO_PIN_12, 0);
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_11|GPIO_PIN_15, 1);
-
-    delay(1000000);
-
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_11|GPIO_PIN_15, 0);
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_10|GPIO_PIN_14, 1);
-
-    delay(1000000);
-}
-
-void apply_odd_even_mode(void) 
-{
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_8|GPIO_PIN_10|GPIO_PIN_12|GPIO_PIN_14, 0);
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_9|GPIO_PIN_11|GPIO_PIN_13|GPIO_PIN_15, 1);
-
-    delay(1000000);
-
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_9|GPIO_PIN_11|GPIO_PIN_13|GPIO_PIN_15, 0);
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_8|GPIO_PIN_10|GPIO_PIN_12|GPIO_PIN_14, 1);
-
-    delay(1000000);
-}
 
 int main(void)
 {
@@ -137,24 +35,135 @@ int main(void)
     }
 }
 
-/**
-  * @brief This function handles EXTI line0 interrupt.
-  */
+mode_function get_mode_function(led_mode_e mode)
+{
+    return mode_functions[mode];
+}
+
+led_mode_e get_current_mode(void)
+{
+    return current_mode;
+}
+
+void set_current_mode(led_mode_e mode)
+{
+    current_mode = mode;
+}
+
+void delay(uint32_t time)
+{
+    while (time--);
+}
+
+static void fill_modes_array()
+{
+    mode_functions[MODE_WHEEL] = apply_wheel_mode;
+    mode_functions[MODE_ALL_ON_OFF] = apply_all_on_off_mode;
+    mode_functions[MODE_SAME_COLOR] = apply_same_color_mode;
+    mode_functions[MODE_ODD_EVEN] = apply_odd_even_mode;
+}
+
+void apply_wheel_mode(void)
+{
+    GPIOE->ODR |= (uint32_t)GPIO_PIN_8;
+    delay(100000);
+    GPIOE->ODR |= (uint32_t)GPIO_PIN_9;
+    delay(100000);
+    GPIOE->ODR |= (uint32_t)GPIO_PIN_10;
+    delay(100000);
+    GPIOE->ODR |= (uint32_t)GPIO_PIN_11;
+    delay(100000);
+    GPIOE->ODR |= (uint32_t)GPIO_PIN_12;
+    delay(100000);
+    GPIOE->ODR |= (uint32_t)GPIO_PIN_13;
+    delay(100000);
+    GPIOE->ODR |= (uint32_t)GPIO_PIN_14;
+    delay(100000);
+    GPIOE->ODR |= (uint32_t)GPIO_PIN_15;
+    delay(100000);
+
+    GPIOE->ODR &= ~((uint32_t)GPIO_PIN_8);
+    delay(100000);
+    GPIOE->ODR &= ~((uint32_t)GPIO_PIN_9);
+    delay(100000);
+    GPIOE->ODR &= ~((uint32_t)GPIO_PIN_10);
+    delay(100000);
+    GPIOE->ODR &= ~((uint32_t)GPIO_PIN_11);
+    delay(100000);
+    GPIOE->ODR &= ~((uint32_t)GPIO_PIN_12);
+    delay(100000);
+    GPIOE->ODR &= ~((uint32_t)GPIO_PIN_13);
+    delay(100000);
+    GPIOE->ODR &= ~((uint32_t)GPIO_PIN_14);
+    delay(100000);
+    GPIOE->ODR &= ~((uint32_t)GPIO_PIN_15);
+    delay(100000);
+}
+
+void apply_all_on_off_mode(void)
+{
+    GPIO_PinState current_state = (GPIOE->IDR && GPIO_PIN_12) == (uint32_t)GPIO_PIN_SET;
+    GPIO_PinState set_state = current_state ? GPIO_PIN_RESET : GPIO_PIN_SET;
+
+    if (set_state)
+    {
+    	GPIOE->ODR |= (uint32_t)GPIO_PIN_All;
+    } else {
+    	GPIOE->ODR &= ~((uint32_t)GPIO_PIN_All);
+    }
+
+    delay(1000000);
+}
+
+void apply_same_color_mode(void)
+{
+    GPIOE->ODR &= ~((uint32_t)(GPIO_PIN_10|GPIO_PIN_14));
+    GPIOE->ODR |= (uint32_t)(GPIO_PIN_9|GPIO_PIN_13);
+
+    delay(1000000);
+
+    GPIOE->ODR &= ~((uint32_t)(GPIO_PIN_9|GPIO_PIN_13));
+    GPIOE->ODR |= (uint32_t)(GPIO_PIN_8|GPIO_PIN_12);
+
+    delay(1000000);
+
+    GPIOE->ODR &= ~((uint32_t)(GPIO_PIN_8|GPIO_PIN_12));
+    GPIOE->ODR |= (uint32_t)(GPIO_PIN_11|GPIO_PIN_15);
+
+    delay(1000000);
+
+    GPIOE->ODR &= ~((uint32_t)(GPIO_PIN_11|GPIO_PIN_15));
+    GPIOE->ODR |= (uint32_t)(GPIO_PIN_10|GPIO_PIN_14);
+
+    delay(1000000);
+}
+
+void apply_odd_even_mode(void)
+{
+    GPIOE->ODR &= ~((uint32_t)(GPIO_PIN_8|GPIO_PIN_10|GPIO_PIN_12|GPIO_PIN_14));
+    GPIOE->ODR |= (uint32_t)(GPIO_PIN_9|GPIO_PIN_11|GPIO_PIN_13|GPIO_PIN_15);
+
+    delay(1000000);
+
+    GPIOE->ODR &= ~((uint32_t)(GPIO_PIN_9|GPIO_PIN_11|GPIO_PIN_13|GPIO_PIN_15));
+    GPIOE->ODR |= (uint32_t)(GPIO_PIN_8|GPIO_PIN_10|GPIO_PIN_12|GPIO_PIN_14);
+
+    delay(1000000);
+}
+
 void EXTI0_IRQHandler(void)
 {
     HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_0);
 
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_All, GPIO_PIN_RESET);
+    GPIOE->ODR &= ~((uint32_t)GPIO_PIN_All);
+
+    led_mode_e current_mode = get_current_mode();
 
     led_mode_e next_mode = current_mode + 1;
 
-    current_mode = next_mode == MODE_CNT ? MODE_WHEEL : next_mode;
+    set_current_mode(next_mode == MODE_CNT ? MODE_WHEEL : next_mode);
 }
 
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
 void SystemClock_Config(void)
 {
     RCC_OscInitTypeDef RCC_OscInitStruct = {0};
@@ -196,8 +205,7 @@ static void MX_GPIO_Init(void)
     __HAL_RCC_GPIOE_CLK_ENABLE();
 
     /*Configure GPIO pin Output Level */
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_8|GPIO_PIN_9|GPIO_PIN_10|GPIO_PIN_11
-                            |GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_All, GPIO_PIN_RESET);
 
     /*Configure GPIO pin : PA0 */
     GPIO_InitStruct.Pin = GPIO_PIN_0;
@@ -207,8 +215,7 @@ static void MX_GPIO_Init(void)
 
     /*Configure GPIO pins : PE8 PE9 PE10 PE11
                              PE12 PE13 PE14 PE15 */
-    GPIO_InitStruct.Pin = GPIO_PIN_8|GPIO_PIN_9|GPIO_PIN_10|GPIO_PIN_11
-                            |GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15;
+    GPIO_InitStruct.Pin = GPIO_PIN_All;
     GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
@@ -219,10 +226,6 @@ static void MX_GPIO_Init(void)
     HAL_NVIC_EnableIRQ(EXTI0_IRQn);
 }
 
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
 void Error_Handler(void)
 {
     __disable_irq();
