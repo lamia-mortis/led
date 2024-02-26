3c216681c99182f64566cd73cf6d514ff438857b
kris <lamia.mortis@icloud.com>
2024-02-26 12:10:58 +0100

    [00000-1] Replacing HAL with registers

--- main.h
+++ main.h
@@ -1,9 +1,7 @@
-/* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __MAIN_H
 #define __MAIN_H
-#endif
 
-#include "stm32f3xx_hal.h"
+#include <stm32f3xx.h>
 
 #define MODE_CNT (4u)
 
@@ -17,6 +15,8 @@ typedef enum {
 } led_mode_e;
 
 mode_function get_mode_function(led_mode_e mode);
+led_mode_e get_current_mode(void);
+void set_current_mode(led_mode_e mode);
 
 void delay(uint32_t time);
 
@@ -24,3 +24,7 @@ void apply_wheel_mode(void);
 void apply_all_on_off_mode(void);
 void apply_same_color_mode(void);
 void apply_odd_even_mode(void);
+
+void Error_Handler(void);
+
+#endif // __MAIN_H
